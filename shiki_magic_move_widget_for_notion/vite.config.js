a83a82daf6a4e7cb834725f3e7b50b47e4d531ac
ldl19691031 <godofpen05@gmail.com>
2025-03-25 20:04:11 +0900

    Try fix github action build error

--- shiki_magic_move_widget_for_notion/vite.config.js
+++ shiki_magic_move_widget_for_notion/vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      crypto: 'crypto-browserify', // 添加 crypto 的别名
     },
+  },
+  define: {
+    'process.env': {}, // 定义 process.env 以避免相关错误
   },
 })
